042b582b06f3eeb316c2d8bd0091d3ec9903647b
Joyce Ng <joyce.ng@oocl.com>
2025-09-14 00:15:48 +0800

    feat: enhance todo app layout and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { useContext, useReducer } from "react";
+import {useContext, useReducer} from "react";
 import "./App.css";
-import { TodoItem } from "./components/TodoItem";
-import { todoReducer } from "./reducers/TodoReducer";
-import { TodoContext } from "./contexts/TodoContext";
+import {TodoItem} from "./components/TodoItem";
+import {todoReducer} from "./reducers/TodoReducer";
+import {TodoContext} from "./contexts/TodoContext";
 
 export const initState = [
-  { id: 1, text: "This is the first thing I need to do", done: false },
-  { id: 2, text: "This is the second thing I need to do", done: false },
-  { id: 3, text: "I already done this item", done: true },
+  {id: 1, text: "This is the first thing I need to do", done: false},
+  {id: 2, text: "This is the second thing I need to do", done: false},
+  {id: 3, text: "I already done this item", done: true},
 ];
 
 function TodoGroup(props) {
-  const { state, dispatch } = useContext(TodoContext);
+  const {state, dispatch} = useContext(TodoContext);
 
   return (
     <div className={"todo-group"}>
       {state.map((item, index) => {
         return (
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <TodoItem todo={item} key={index} index={index} />
+          <div style={{display: "flex", alignItems: "center"}} key={item.id}>
+            <TodoItem todo={item} key={index} index={index}/>
             <button
               className={"todo-delete"}
               onClick={() =>
-                dispatch({ type: "DELETE_TODO", payload: { id: item.id } })
+                dispatch({type: "DELETE_TODO", payload: {id: item.id}})
               }
             >
               X
@@ -37,22 +37,25 @@ function TodoGroup(props) {
 function App() {
   const [state, dispatch] = useReducer(todoReducer, initState);
   return (
-    <div style={{ width: "fit-content" }}>
-      <TodoContext.Provider value={{ state, dispatch }}>
-        <TodoGroup />
-      </TodoContext.Provider>
-      <div className="todo-add-wrapper">
-        <input className={"todo-input"} type={"text"} />
-        <button
-          className={"todo-add"}
-          onClick={() => {
-            const input = document.querySelector(".todo-input");
-            dispatch({ type: "ADD_TODO", payload: { text: input.value } });
-            input.value = "";
-          }}
-        >
-          add
-        </button>
+    <div className="app-container">
+      <h2>Todo List</h2>
+      <div className="todo-container">
+        <TodoContext.Provider value={{state, dispatch}}>
+          <TodoGroup/>
+        </TodoContext.Provider>
+        <div className="todo-add-container">
+          <input className={"todo-input"} type={"text"}/>
+          <button
+            className={"todo-add"}
+            onClick={() => {
+              const input = document.querySelector(".todo-input");
+              dispatch({type: "ADD_TODO", payload: {text: input.value}});
+              input.value = "";
+            }}
+          >
+            add
+          </button>
+        </div>
       </div>
     </div>
   );
