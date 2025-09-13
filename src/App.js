c69c60d144237c9e326966cbe76a668216b92b92
Joyce Ng <joyce.ng@oocl.com>
2025-09-14 00:31:06 +0800

    refactor: extract TodoGroup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import {useContext, useReducer} from "react";
+import {useReducer} from "react";
 import "./App.css";
-import {TodoItem} from "./components/TodoItem";
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
+import {TodoGroup} from "./components/TodoGroup";
 
 export const initState = [
   {id: 1, text: "This is the first thing I need to do", done: false},
   {id: 2, text: "This is the second thing I need to do", done: false},
   {id: 3, text: "I already done this item", done: true},
 ];
-
-function TodoGroup() {
-  const {state, dispatch} = useContext(TodoContext);
-
-  return (
-    <div className={"todo-group"}>
-      {state.map((item, index) => {
-        return (
-          <div style={{display: "flex", alignItems: "center"}} key={item.id}>
-            <TodoItem todo={item} key={index} index={index}/>
-            <button
-              className={"todo-delete"}
-              onClick={() =>
-                dispatch({type: "DELETE_TODO", payload: {id: item.id}})
-              }
-            >
-              x
-            </button>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
 
 function App() {
   const [state, dispatch] = useReducer(todoReducer, initState);
---
+++ src/components/TodoGroup.js
@@ -0,0 +1,27 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {TodoItem} from "./TodoItem";
+
+export function TodoGroup() {
+  const {state, dispatch} = useContext(TodoContext);
+
+  return (
+    <div className={"todo-group"}>
+      {state.map((item, index) => {
+        return (
+          <div style={{display: "flex", alignItems: "center"}} key={item.id}>
+            <TodoItem todo={item} key={index} index={index}/>
+            <button
+              className={"todo-delete"}
+              onClick={() =>
+                dispatch({type: "DELETE_TODO", payload: {id: item.id}})
+              }
+            >
+              x
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
