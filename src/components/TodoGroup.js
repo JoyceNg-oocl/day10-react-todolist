5a8de5b6dc87ead25ca43704feecdcb1c9cf9fed
Joyce Ng <joyce.ng@oocl.com>
2025-09-14 00:52:26 +0800

    feat: display message when no todo item

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -7,21 +7,25 @@ export function TodoGroup() {
 
   return (
     <div className={"todo-group"}>
-      {state.map((item, index) => {
-        return (
-          <div style={{display: "flex", alignItems: "center"}} key={item.id}>
-            <TodoItem todo={item} key={index} index={index}/>
-            <button
-              className={"todo-delete"}
-              onClick={() =>
-                dispatch({type: "DELETE_TODO", payload: {id: item.id}})
-              }
-            >
-              x
-            </button>
-          </div>
-        );
-      })}
+      {state.length === 0 ? (
+        <div>Add the things you need to do today...</div>
+      ) : (
+        state.map((item, index) => {
+          return (
+            <div style={{display: "flex", alignItems: "center"}} key={item.id}>
+              <TodoItem todo={item} key={index} index={index}/>
+              <button
+                className={"todo-delete"}
+                onClick={() =>
+                  dispatch({type: "DELETE_TODO", payload: {id: item.id}})
+                }
+              >
+                x
+              </button>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
