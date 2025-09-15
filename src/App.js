14d968a60126bf8ee9cf1e7c1d0578d71bd6408a
Joyce Ng <joyce.ng@oocl.com>
2025-09-15 10:31:50 +0800

    feat: import react router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
+import {createBrowserRouter, RouterProvider} from "react-router";
 
 export const initState = [
   {id: 1, text: "This is the first thing I need to do", done: false},
   {id: 2, text: "This is the second thing I need to do", done: false},
   {id: 3, text: "I already done this item", done: true},
 ];
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <TodoList/>,
+  },
+]);
 
 function App() {
   const [state, dispatch] = useReducer(todoReducer, initState);
@@ -17,7 +25,7 @@ function App() {
       <h2>Todo List</h2>
       <div className="todo-container">
         <TodoContext.Provider value={{state, dispatch}}>
-          <TodoList/>
+          <RouterProvider router={routes}/>
         </TodoContext.Provider>
       </div>
     </div>
