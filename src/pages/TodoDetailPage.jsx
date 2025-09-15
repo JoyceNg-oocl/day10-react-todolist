85388b96f9d50c746a037e3af1749a7ceb3cd337
Joyce Ng <joyce.ng@oocl.com>
2025-09-15 17:00:19 +0800

    feat: add dropdown for selecting todos in detail page

--- src/pages/TodoDetailPage.jsx
+++ src/pages/TodoDetailPage.jsx
@@ -1,19 +1,41 @@
+import React, {useContext} from "react";
 import {useParams} from "react-router";
-import {useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
 import {TodoItem} from "../components/TodoItem";
+import {Link} from "react-router-dom";
+import {Dropdown, Space} from "antd";
+import {DownOutlined} from "@ant-design/icons";
 
 export function TodoDetailPage() {
   const {id} = useParams();
   const {state} = useContext(TodoContext);
   const todo = state.filter((todo) => todo.id === id);
 
+  const dropdownItems = state.map((todo) => ({
+    key: todo.id,
+    label: (
+      <Link to={`/todos/${todo.id}`}>
+        {todo.text}
+      </Link>
+    ),
+  }));
+
   if (todo.length === 0) {
     return <div>Todo item with id {id} not found.</div>;
   }
 
-  return <div>
-    <h2 style={{textAlign: "center"}}>Todo Detail</h2>
-    <TodoItem todo={todo[0]} index={id}/>
-  </div>;
+  return (
+    <div>
+      <h2 style={{textAlign: "center"}}>Todo Detail</h2>
+      <Dropdown menu={{items: dropdownItems}}>
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            Select Todo
+            <DownOutlined/>
+          </Space>
+        </a>
+      </Dropdown>
+      <TodoItem todo={todo[0]} index={id}/>
+    </div>
+  );
 }
