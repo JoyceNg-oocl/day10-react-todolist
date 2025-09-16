d2b012e71699687d6aaa27f12c9b93bcb9857fcd
Joyce Ng <joyce.ng@oocl.com>
2025-09-16 10:31:41 +0800

    feat: add response interceptor for error handling

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,25 @@
 import axios from "axios";
+import {message} from "antd";
 
 export const api = axios.create({
   baseURL: "https://68c7ac8e5d8d9f514732879a.mockapi.io/",
   headers: {"Content-Type": "application/json"},
   timeout: 10000,
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const {status} = error.response;
+    if (status === 404) {
+      alert("Resource not found (404)");
+      // message.error({
+      //   content: error.message,
+      //   duration: 10,
+      // });
+    }
+    return Promise.reject(error);
+  }
+);
