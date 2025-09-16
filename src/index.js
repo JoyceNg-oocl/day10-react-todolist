39fff76b1b6371c52c02882f31a4cbf31ab8f4bd
Joyce Ng <joyce.ng@oocl.com>
2025-09-16 17:45:26 +0800

    feat: add Ant Design render patch for React 19

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {unstableSetRender} from 'antd';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+unstableSetRender((node, container) => {
+  container._reactRoot ||= ReactDOM.createRoot(container);
+  const root = container._reactRoot;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
